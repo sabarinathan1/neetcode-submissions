bb1f5607252ce6fbd7d78653661957b00afa309d
Sabari <mail2sabariksr@gmail.com>
2026-09-06 06:22:56 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        //Lets use two pointers here to iterate quick
+        /*
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = nums.length - 1; j > i; j--){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+        */
+        /*
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        */
+        Set<Integer> set  = new HashSet<Integer>();
+        for(int num : nums) {
+            if(!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
